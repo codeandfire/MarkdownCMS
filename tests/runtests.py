ac43466e10d801cdb733c073c1a5ff784c85c2e9
Saniya Maheshwari <saniya.mah@gmail.com>
2023-05-20 22:15:57 +0530

    Improvements to test runner script.
      - Recompiles before running tests.
      - Individual characters are displayed separated by single spaces.
      - Whitespace characters are displayed more clearly: space is shown
        as a solid dot, and `\t`, `\r` and `\n` are shown as their respective
        escape sequences.

--- tests/runtests.py
+++ tests/runtests.py
@@ -1,25 +1,41 @@
 #!/bin/python3
 
+import sys
 import json
 import textwrap
 import subprocess
+import unicodedata
 from pathlib import Path
 from collections import namedtuple
 from difflib import SequenceMatcher
 
-def display(text, textwidth=70, label=None, labelwidth=10, marks=None, markchar='^'):
+def display(
+        text, textwidth=90,
+        label=None, labelwidth=20,
+        marks=None, markchar='^',
+        showblanks=True,
+        spaceout=True, spacechar=unicodedata.lookup('BLACK CIRCLE')):
     """
-    label          text ......................................
-                    ^  ^     ^   ^^^^^^^^^^     ^^^^^^   marks
-                   ...........................................
-                       ^^^^^^^^     ^^^^^^^^^^     ^^^ ^^    ^
-                   ...........................................
-                   ^^^^^^^   ^^^^         ^^^^^^       ^^^^^^^
-                   ...........................................
-                               ^^^^^^^^^^^^^^^^^^^^^^^^^    ^^
+    <label>        ........................................... <text>
+                    ^  ^     ^   ^^^^^^^^^^     ^^^^^^         <marks>
+                   ........................................... <text>
+                       ^^^^^^^^     ^^^^^^^^^^     ^^^ ^^    ^ <marks>
+                   ........................................... <text>
+                   ^^^^^^^   ^^^^         ^^^^^^       ^^^^^^^ <marks>
+                   ........................................... <text>
+                               ^^^^^^^^^^^^^^^^^^^^^^^^^    ^^ <marks>
 
-    | ----------- || --------------------------------------- |
+    | ----------- || ---------------------------------------- |
       labelwidth                    textwidth
+
+    with spaceout :
+
+    <label>        . . . . . . . . . . . . . . . . . . . . . . . <text>
+                     ^     ^     ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^     ^     <marks>
+                   . . . . . . . . . . . . . . . . . . . . . . . <text>
+                       ^ ^ ^ ^ ^ ^             ^ ^ ^   ^ ^     ^ <marks>
+
+    i.e. each character in the text and the marks is separated by a single space.
     """
 
     def nspaces(n):
@@ -32,30 +48,49 @@ def display(text, textwidth=70, label=None, labelwidth=10, marks=None, markchar=
         labelwidth = 0
     if marks is not None:
         assert len(text) == len(marks)    # as many marks as number of characters in text
-    if text == '':
-        text = '<blank>'
-        marks = None
+        marks = [(markchar if mark else ' ') for mark in marks]
 
-    text = label + nspaces(labelwidth - len(label)) + text
-    text = textwrap.wrap(
-        text,
-        width=(labelwidth + textwidth),
-        initial_indent='',
-        subsequent_indent=nspaces(labelwidth),
-        drop_whitespace=False)
+    text = list(text)
+    if showblanks:
+        if marks is not None:
+            # marks corresponding to \t, \n or \r need an extra space after the mark
+            # because they will now correspond to two characters (\\t, \\n, \\r) instead
+            # of one.
+            marks = [
+                mark + (' ' if char in ('\t', '\n', '\r') else '')
+                for mark, char in zip(marks, text)
+            ]
+        for old, new in zip(
+                (' ', '\t', '\n', '\r'), (spacechar, '\\t', '\\n', '\\r')):
+            text = [new if char == old else char for char in text]
+    text = (' ' if spaceout else '').join(text)
     if marks is not None:
-        marks = ''.join([markchar if mark else ' ' for mark in marks])
+        marks = (' ' if spaceout else '').join(marks)
+
+    text = textwrap.wrap(
+        label + nspaces(labelwidth - len(label)) + text,  # we add the label as part of the text for easy formatting
+        width=(labelwidth + textwidth),
+        drop_whitespace=False,  # NOTE: consider replace_whitespace, expand_tabs, tabsize?
+        initial_indent=nspaces(0), subsequent_indent=nspaces(labelwidth))
+    if marks is not None:
         marks = textwrap.wrap(
             marks,
             width=(labelwidth + textwidth),
-            initial_indent=nspaces(labelwidth),
-            subsequent_indent=nspaces(labelwidth),
-            drop_whitespace=False)
-        assert len(text) == len(marks)     # number of lines of text and marks has to be the same
-        for i in range(len(marks)):        # insert the marks lines in between the text lines
-            text.insert(2*i + 1, marks[i]);
-    for line in text:
-        print(line)
+            drop_whitespace=False,  # same NOTE as before
+            initial_indent=nspaces(labelwidth), subsequent_indent=nspaces(labelwidth))
+        assert len(marks) == len(text)      # both must have the same number of lines
+        # append the marks lines after their corresponding text lines
+        text = [
+            text_line + '\n' + marks_line for text_line, marks_line in zip(text, marks)]
+    print('\n'.join(text))
+
+try:                        # recompile before running tests
+    subprocess.run(
+        ['cc', Path(__file__).parent.parent.joinpath('translate.c')],
+        text=True, check=True)
+except subprocess.CalledProcessError:
+    print('Compilation failed.', file=sys.stderr)
+    sys.exit(1)             # exit with code 1 if compilation fails
 
 matcher = SequenceMatcher()
 Testcase = namedtuple('Testcase', ['stdin', 'stdout', 'stderr'])
@@ -63,6 +98,7 @@ Testcase = namedtuple('Testcase', ['stdin', 'stdout', 'stderr'])
 with Path(__file__).parent.joinpath('testcases.json').open() as f:
     testcases = json.load(f)
 
+count = 0
 for idx, case in enumerate(testcases):
     case = Testcase(**case)
     run_obj = subprocess.run(
@@ -71,6 +107,7 @@ for idx, case in enumerate(testcases):
         text=True,
         capture_output=True)
     if run_obj.stdout == case.stdout and run_obj.stderr == case.stderr:
+        count += 1
         continue
 
     print(f'Case #{idx+1}')
@@ -79,12 +116,16 @@ for idx, case in enumerate(testcases):
         sentA, sentB = getattr(run_obj, field), getattr(case, field)
         if sentA == sentB:
             continue
-        marksA = [True for _ in range(len(sentA))]
+        marksA = [True for _ in range(len(sentA))]  # initially assume all characters are marked
         marksB = [True for _ in range(len(sentB))]
         matcher.set_seqs(sentA, sentB)
         for block in matcher.get_matching_blocks():
+            # the sequences belonging to matching blocks are correct and should not be marked
             marksA[block.a : block.a + block.size] = [False for _ in range(block.size)]
             marksB[block.b : block.b + block.size] = [False for _ in range(block.size)]
         display(text=sentA, label=field, marks=marksA)
         display(text=sentB, label='expected', marks=marksB)
     print('---')
+
+print(f'{count}/{idx+1} passed.')
+sys.exit(0 if count == idx+1 else 2);   # exit with code 2 if some tests fail
