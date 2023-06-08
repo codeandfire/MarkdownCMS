3ebf1caab9a60b0e2a6ae8996e6d850b00eaaa04
Saniya Maheshwari <saniya.mah@gmail.com>
2023-06-08 17:35:26 +0530

    Implemented `dumpstate()`.
     - `HEADING_NUMBER` changed to `HEADING_LEVEL`.

--- translate.c
+++ translate.c
@@ -11,8 +11,8 @@ struct {
 	int colno;
 
 	/* states of individual elements */
-	enum { NO_HEADING, HEADING_NUMBER, HEADING_TEXT } heading;
-	enum { ONE_SPACE, TWO_SPACES, ONE_TAB, NO_WHITESPACE } whitespace;
+	enum { NO_HEADING, HEADING_LEVEL, HEADING_TEXT } heading;
+	enum { NO_WHITESPACE, ONE_SPACE, TWO_SPACES, ONE_TAB } whitespace;
 
 	int heading_level;			/* level of current heading (1, 2, 3, 4, 5, 6) */
 	bool syntax;				/* current character is a syntax character or not */
@@ -28,7 +28,7 @@ int error(FILE *, enum error_type, ...);
 enum warning_type { MULTIPLE_SPACES_USED, TAB_USED };
 void warning(FILE *, enum warning_type);
 
-void dumpstate(char);
+void dumpstate(FILE *, char);
 
 const int MAX_HEADING_LEVEL = 6;
 
@@ -74,9 +74,9 @@ int translate(FILE *fin, FILE *fout, FILE *ferr, struct options *popt)
 			case '#':
 				if (state.colno == 1) {
 					state.heading_level = 1;
-					state.heading = HEADING_NUMBER;
+					state.heading = HEADING_LEVEL;
 				}
-				else if (state.heading == HEADING_NUMBER) {
+				else if (state.heading == HEADING_LEVEL) {
 					if (++state.heading_level > MAX_HEADING_LEVEL)
 						return error(ferr, HEADING_LEVEL_TOO_HIGH, MAX_HEADING_LEVEL);
 				}
@@ -85,7 +85,7 @@ int translate(FILE *fin, FILE *fout, FILE *ferr, struct options *popt)
 				break;
 
 			case ' ':
-				if (state.heading == HEADING_NUMBER) {
+				if (state.heading == HEADING_LEVEL) {
 					html(fprintf(fout, "<h%d>", state.heading_level));
 					state.heading = HEADING_TEXT;
 				}
@@ -112,7 +112,7 @@ int translate(FILE *fin, FILE *fout, FILE *ferr, struct options *popt)
 			}
 
 		if (popt->debug)
-			dumpstate(c);
+			dumpstate(fout, c);
 		else if (!state.syntax && c != EOF)
 			html(putc(c, fout));				/* echo the character */
 	}
@@ -167,8 +167,44 @@ void warning(FILE *ferr, enum warning_type wt)		/* warnings don't have any assoc
 	fprintf(ferr, "\n");
 }
 
-void dumpstate(char c)
+void dumpstate(FILE *fout, char c)
 {
-	/* TODO */
-	printf("dump\n");
+	static bool first_call = true;					/* first call to dumpstate() i.e. the input has just started */
+
+	char *heading_str[] = { "NO_HEADING", "HEADING_LEVEL", "HEADING_TEXT" };
+	char *whitespace_str[] = { "NO_WHITESPACE", "ONE_SPACE", "TWO_SPACES", "ONE_TAB" };
+
+	if (first_call) {
+		putc('[', fout);
+		first_call = false;
+	}
+	putc('{', fout);
+
+#define	dump_int(KEY, INT)			fprintf(fout, "\"" KEY "\":%d,", INT)
+#define	dump_bool(KEY, BOOL)		fprintf(fout, "\"" KEY "\":%s,", BOOL ? "true" : "false")
+#define	dump_str(KEY, STR)			fprintf(fout, "\"" KEY "\":\"%s\",", STR)
+
+	/* the above dump_ macros print the key-value pair with a comma following it, assuming that another key-value pair
+	 * comes next
+	 * JSON is strict about commas and the Python decoder will complain if there is a comma but no following element
+	 * the below macro dumps a bool (the last element to be printed is hit_eof which is a bool) without this comma */
+
+#define	dump_bool_last(KEY, BOOL)	fprintf(fout, "\"" KEY "\":%s", BOOL ? "true" : "false")
+
+	dump_int("charcode", c);						/* printing integer value of character is less tedious/ambiguous
+													   than printing its character form (have to handle escape
+													   sequences, non-printable characters, etc.) */
+	dump_int("lineno", state.lineno);
+	dump_int("colno", state.colno);
+	dump_str("heading", heading_str[state.heading]);
+	dump_str("whitespace", whitespace_str[state.whitespace]);
+	dump_int("heading_level", state.heading_level);
+	dump_bool("syntax", state.syntax);
+	dump_bool_last("hit_eof", state.hit_eof);
+
+	putc('}', fout);
+	if (c != EOF)
+		putc(',', fout);
+	else
+		putc(']', fout);
 }
